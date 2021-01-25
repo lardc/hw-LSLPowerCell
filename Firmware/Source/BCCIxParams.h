192f56943aea68cb50e18e1ae49da84634b66625
VShurdov <v.shurdov@proton-electrotex.com>
2021-01-26 01:35:28 +0300

    Исправлена ошибка в написании CAN_SLAVE_NID

--- Firmware/Source/BCCIxParams.h
+++ Firmware/Source/BCCIxParams.h
@@ -13,7 +13,7 @@
 
 // Конфигурация Slave
 //
-#define CAN_SALVE_NID				150				// Node ID устройства
+#define CAN_SLAVE_NID				150				// Node ID устройства
 #define CAN_SLAVE_NID_MASK			0x0003FC00		// Маска Slave node ID
 #define CAN_SLAVE_NID_MPY			10				// Смещение адреса Slave
 
@@ -25,7 +25,7 @@
 
 // CAN фильтр
 //
-#define CAN_SLAVE_FILTER_ID				(CAN_SALVE_NID << CAN_SLAVE_NID_MPY)
+#define CAN_SLAVE_FILTER_ID				(CAN_SLAVE_NID << CAN_SLAVE_NID_MPY)
 #define CAN_MASTER_FILTER_ID			(CAN_MASTER_NID << CAN_MASTER_NID_MPY)
 
 #endif // __BCCI_PARAMS_H
